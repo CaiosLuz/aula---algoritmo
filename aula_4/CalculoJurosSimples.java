c37bad6f2ab9db8cc88327a0fefa02591d57bb66
CaiosLuz <125562747+CaiosLuz@users.noreply.github.com>
2024-03-24 21:52:05 -0300

    Exercícios concluidos

--- aula_4/CalculoJurosSimples.java
+++ aula_4/CalculoJurosSimples.java
@@ -12,10 +12,14 @@ public class CalculoJurosSimples {
         System.out.println("Digite o valor da taxa de juros");
         double taxa = scanner.nextDouble();
 
+        // Converter a taxa para decimal para realizar a operação
+
         double taxaDecimal = taxa / 100;
 
         System.out.println("Digite o tempo de aplicação em anos");
         double tempo = scanner.nextDouble();
+
+        // Fórmula: J (juros) = C (capital inicial) * I (taxa/juros) * T (tempo)
 
         double juros = capital * taxaDecimal * tempo;
 
